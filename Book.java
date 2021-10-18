5497c63c38b71b18d2a0660f8ea180c0b77390c4
yuma0909 <92625869+yuma0909@users.noreply.github.com>
2021-10-18 18:55:55 +0900

    Revert "Create Book.java"

--- Book.java
+++
@@ -1,9 +0,0 @@
-public class Book extends TangibleAsset{
-	private String isbn;
-	public Book (String name, int price, String color, String isbn){
-		super (name, price, color);
-		this.isbn = isbn;
-	}
-
-	public String getIsbn(){ return this.Isbn;}
-}
